80707c04a7c801051c429da489316a2f21f14e86
Zhicheng Wu <zhicheng.wu@columbia.edu>
2018-11-16 16:00:03 -0500

    add core

---
+++ qalitesite/core.py
@@ -0,0 +1,13 @@
+def get_title(classid):
+    return alltitle
+
+def get_username(email):
+    return username
+
+def get_question_detail(q_id):
+    comment = get_comment(q_id)
+
+    return question_detail
+
+def get_comment(q_id):
+    return comments
